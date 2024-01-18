18f9d5e9dca6439c684c70cd96cdbb1041fab5c7
kateramckay <kateradmckay@gmail.com>
2024-01-18 10:17:12 -0500

    adding a python file

---
+++ bank.py
@@ -0,0 +1,27 @@
+import os
+
+def main():
+    account_type = None
+    balance = None
+    amount = None
+    first_name = None
+    last_name = None
+
+    def open_account():
+        pass
+
+    def credentials():
+        pass
+
+    def deposit_money():
+        pass
+
+    def withdraw_money():
+        pass
+
+    def account_display():
+        pass
+
+
+if __name__ == '__main__':
+    main()
